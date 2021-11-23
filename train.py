c96c341081eea23ffc49cc29fefad70d93e1dba2
Mriganka Nath <mrinath123@gmail.com>
2021-11-23 23:45:56 +0530

    delete duplicates

--- train.py
+++ train.py
@@ -316,6 +316,7 @@ if __name__ == '__main__':
     
     set_seed(42)
 
+
     print("Training on:", config['PET_CLASS'])
     print("Starting on Fold", config['START_FOLD'])
     if config['PET_CLASS'] == "cat" or config['PET_CLASS'] == "dog":
@@ -325,6 +326,45 @@ if __name__ == '__main__':
         df = df.drop('pet_class', axis=1)
     else:
         df = pd.read_csv(f"{config['DATA_DIR']}train.csv")
+
+    to_be_removed = ['13d215b4c71c3dc603cd13fc3ec80181',
+                    '5ef7ba98fc97917aec56ded5d5c2b099',
+                    '1feb99c2a4cac3f3c4f8a4510421d6f5',
+                    '5a642ecc14e9c57a05b8e010414011f2',
+                    '0422cd506773b78a6f19416c98952407',
+                    '9b3267c1652691240d78b7b3d072baf3',
+                    '1059231cf2948216fcc2ac6afb4f8db8',
+                    '8ffde3ae7ab3726cff7ca28697687a42',
+                    '78a02b3cb6ed38b2772215c0c0a7f78e',
+                    'bf8501acaeeedc2a421bac3d9af58bb7',
+                    'fe47539e989df047507eaa60a16bc3fd',
+                    'dd042410dc7f02e648162d7764b50900',
+                    '988b31dd48a1bc867dbc9e14d21b05f6',
+                    'e359704524fa26d6a3dcd8bfeeaedd2e',
+                    '6ae42b731c00756ddd291fa615c822a1',
+                    '9a0238499efb15551f06ad583a6fa951',
+                    'a9513f7f0c93e179b87c01be847b3e4c',
+                    '38426ba3cbf5484555f2b5e9504a6b03',
+                    'cd909abf8f425d7e646eebe4d3bf4769',
+                    '9f5a457ce7e22eecd0992f4ea17b6107',
+                    'b148cbea87c3dcc65a05b15f78910715',
+                    '3877f2981e502fe1812af38d4f511fd2',
+                    'b190f25b33bd52a8aae8fd81bd069888',
+                    '94c823294d542af6e660423f0348bf31',
+                    '2b737750362ef6b31068c4a4194909ed',
+                    '01430d6ae02e79774b651175edd40842',
+                    '72b33c9c368d86648b756143ab19baeb',
+                    'dbc47155644aeb3edd1bd39dba9b6953',
+                    'b49ad3aac4296376d7520445a27726de',
+                    '54563ff51aa70ea8c6a9325c15f55399',
+                    '87c6a8f85af93b84594a36f8ffd5d6b8',
+                    '16d8e12207ede187e65ab45d7def117b']
+    
+    for i in to_be_removed:
+        try:
+            df.drop(df[df.Id == i].index.values[0],axis=0,inplace=True)
+        except IndexError:
+            pass
 
 
     y = df.Pawpularity.values
@@ -340,8 +380,8 @@ if __name__ == '__main__':
         [A.RandomResizedCrop(config['IMAGE_SIZE'],config['IMAGE_SIZE'],p = config['CROP']),
             A.Resize(config['IMAGE_SIZE'],config['IMAGE_SIZE'],p = config['RESIZE']),
             A.HorizontalFlip(p = config['H_FLIP']),  
-             A.VerticalFlip(p=0.5),   
-            A.Transpose(p=0.5), 
+             A.VerticalFlip(p=0.3),   
+            A.Transpose(p=0.3), 
             A.RandomBrightnessContrast(p = config['BRIGHT_CONTRAST']),
             A.HueSaturationValue(
                 hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
