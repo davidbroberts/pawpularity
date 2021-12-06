8a22ae74a2f9b4323993ee79c6ce7570143d5ae9
Mriganka Nath <mrinath123@gmail.com>
2021-12-06 20:33:49 +0530

    tweaks

--- train.py
+++ train.py
@@ -112,7 +112,7 @@ class Pawpu(Sampler):
 class Model(nn.Module):
     def __init__(self,pretrained):
         super().__init__()
-        self.backbone = timm.create_model(config['MODEL_NAME'], pretrained=True, num_classes=0, drop_rate=0.2, drop_path_rate=0.2,global_pool='')
+        self.backbone = timm.create_model(config['MODEL_NAME'], pretrained=True, num_classes=0, drop_rate=0.0, drop_path_rate=0.0,global_pool='')
         self.pool = nn.AdaptiveAvgPool2d(1)
         self.fc3_A = nn.Linear(config['NUM_NEURONS'],12)
         self.fc3_B = nn.Linear(config['NUM_NEURONS'],1)
@@ -400,12 +400,12 @@ if __name__ == '__main__':
             A.Resize(config['IMAGE_SIZE'],config['IMAGE_SIZE'],p = config['RESIZE']),
             A.HorizontalFlip(p = config['H_FLIP']),  
              A.VerticalFlip(p=0.5),   
-            A.Transpose(p=0.5), 
+            A.Transpose(p=0.3), 
             A.RandomBrightnessContrast(p = config['BRIGHT_CONTRAST']),
             A.HueSaturationValue(
                 hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
             A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=30, p=0.5),
-            A.Cutout(max_h_size=int(config['IMAGE_SIZE'] * 0.125), max_w_size=int(config['IMAGE_SIZE'] * 0.125), num_holes=7, p=0.5),
+            A.Cutout(max_h_size=int(config['IMAGE_SIZE'] * 0.125), max_w_size=int(config['IMAGE_SIZE'] * 0.125), num_holes=6, p=0.5),
               
        A.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
             ToTensorV2()
