895449bf7a5dac3bc2e3673224c4c5ea153fd449
Mriganka Nath <mrinath123@gmail.com>
2021-12-21 21:48:01 +0530

    tweak

--- train.py
+++ train.py
@@ -273,13 +273,13 @@ def fit(m, fold_n, training_batch_size = config['TRAIN_BATCH_SIZE'], validation_
     criterion= nn.BCEWithLogitsLoss()
     #optimizer = optim.AdamW(m.parameters(), lr = config['LR'], weight_decay = config['WEIGHT_DECAY'])
     base_optimizer = optim.AdamW # define an optimizer for the "sharpness-aware" update
-    optimizer = SAM(m.parameters(), base_optimizer, lr=config['LR'] , weight_decay =config['WEIGHT_DECAY'])
+    optimizer = SAM(m.parameters(), base_optimizer, lr=2e-5 , weight_decay = 1e-6)
     
     wandb.watch(model, criterion, log="all", log_freq=10)
     
   
     num_train_steps = math.ceil(len(train_loader))
-    num_warmup_steps = num_train_steps * (config['EPOCHS']//2 -1)
+    num_warmup_steps = num_train_steps * (config['EPOCHS']//2 )
     num_training_steps = int(num_train_steps * config['EPOCHS'])
     sch = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps = num_warmup_steps, num_training_steps = num_training_steps) 
     
@@ -330,26 +330,37 @@ if __name__ == '__main__':
         df = pd.read_csv(f"{config['DATA_DIR']}train.csv")
 
 
-    y = df.Pawpularity.values
+    '''y = df.Pawpularity.values
     kf = model_selection.StratifiedKFold(n_splits = config['NUM_FOLDS'], random_state=42, shuffle=True)
     
     for f,(t,v) in enumerate(kf.split(X=df,y=y)):
         df.loc[v,'fold'] = f
+    
+    num_bins = int(np.ceil(2*((len(df))**(1./3))))
+    df['bins'] = pd.cut(df['Pawpularity'], bins=num_bins, labels=False)
+    df['fold'] = -1'''
+
+    
+    strat_kfold = model_selection.StratifiedKFold(n_splits=config['NUM_FOLDS'], random_state=42, shuffle=True)
+    for i, (_, train_index) in enumerate(strat_kfold.split(df.index, df['bins'])):
+        df.iloc[train_index, -1] = i
+        
+    df['fold'] = df['fold'].astype('int')
 
     df['path'] = [f"{config['TRAIN_IMAGES_PATH']}{x}.jpg" for x in df["Id"].values]
 
     
     train_aug = A.Compose(
-        [A.RandomResizedCrop(config['IMAGE_SIZE'],config['IMAGE_SIZE'],p = config['CROP']),
+        [
             A.Resize(config['IMAGE_SIZE'],config['IMAGE_SIZE'],p = config['RESIZE']),
             A.HorizontalFlip(p = config['H_FLIP']),  
              A.VerticalFlip(p=0.5),   
-            A.Transpose(p=0.3), 
+            
             A.RandomBrightnessContrast(p = config['BRIGHT_CONTRAST']),
             A.HueSaturationValue(
                 hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
             A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=30, p=0.5),
-            A.Cutout(max_h_size=int(config['IMAGE_SIZE'] * 0.125), max_w_size=int(config['IMAGE_SIZE'] * 0.125), num_holes=6, p=0.5),
+            A.Cutout(max_h_size=int(config['IMAGE_SIZE'] * 0.21), max_w_size=int(config['IMAGE_SIZE'] * 0.21), num_holes=4, p=0.5),
               
        A.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
             ToTensorV2()
